4225f6e345441d401244bd39ab26ba6bdcc9d817
Jason <jason.t.ward@gmail.com>
2024-01-23 12:24:30 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Aspose.Cells;
+using AsposeCells.DigitalSigning.Test;
+
+Console.WriteLine("Hello, World!");
+
+//TODO:Upload file and output signed file
+//TODO:Utilize Aspose cells workbook.VbaProject.Sign with pfx cert file
+
+var workbook = WorkbookExtensions.CreateWorkbook();
+var digitalSignature = WorkbookExtensions.GetDigitalSignature();
+workbook.VbaProject.Sign(digitalSignature);
+var stream = new MemoryStream();
+workbook.Save(stream, SaveFormat.Xlsm);
+MyStream.Main(stream.ToArray());
---
+++ WorkbookExtensions.cs
@@ -0,0 +1,67 @@
+﻿using Aspose.Cells;
+using Aspose.Cells.DigitalSignatures;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AsposeCells.DigitalSigning.Test
+{
+    public class WorkbookExtensions
+    {
+        public static Workbook CreateWorkbook()
+        {
+            var loadOptions = new LoadOptions(LoadFormat.Auto)
+            {
+                MemorySetting = MemorySetting.MemoryPreference
+            };
+
+            const string path = @"Contents\Book1.xlsm";
+
+            var readText = File.ReadAllBytes(path);
+            var stream = new MemoryStream(readText);
+
+            var workbook = new Workbook(stream, loadOptions);
+            return workbook;
+        }
+
+        public static DigitalSignature GetDigitalSignature()
+        {
+            const string comment = "Signing Digital Signature using Aspose.Cells";
+
+            const string pfxPath = @"Contents\friendly-cert2.pfx";
+
+            var cert = new X509Certificate2(pfxPath, "mypassword", X509KeyStorageFlags.MachineKeySet);
+
+            var digitalSignature =
+                new DigitalSignature
+                    (cert, comment, DateTime.UtcNow);
+            return digitalSignature;
+        }
+    }
+
+    public class MyStream
+    {
+        private const string FileName = "Test.xlsm";
+
+        public static void Main(byte[] content)
+        {
+            if (File.Exists(FileName))
+            {
+                Console.WriteLine($"{FileName} already exists!");
+                return;
+            }
+
+            using (var fs = new FileStream(FileName, FileMode.CreateNew))
+            {
+                fs.Write(content, 0, content.Length);
+            }
+
+            using (var fs = new FileStream(FileName, FileMode.Open, FileAccess.Read))
+            {
+                using var r = new BinaryReader(fs);
+                for (var i = 0; i < 11; i++)
+                {
+                    Console.WriteLine(r.ReadInt32());
+                }
+            }
+        }
+    }
+}
